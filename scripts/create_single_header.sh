d80dbefd2a63d6ca53115d7f4452c49b1ff1c1aa
DmitriBogdanov <dm.bogdanov2000@gmail.com>
2025-01-29 16:51:38 +0300

    chore(): Preparing files for a repo rename.

--- scripts/create_single_header.sh
+++ scripts/create_single_header.sh
@@ -1,5 +1,5 @@
 input_folder="source/*.hpp"
-output_file="include/proto_utils.hpp"
+output_file="include/UTL.hpp"
 
 for f in $input_folder; do
     [[ "$f" = $output_file ]] || { cat -- "$f"; printf "\n\n\n\n\n\n"; }
